bd6652fa6e1b637b3fb88ae21523909b95b0f3c3
blessingcharles <charlesblessing001@gmail.com>
2022-04-15 10:24:15 +0530

    pcapng added

---
+++ utilities/smb/wireshark-gen.sh
@@ -0,0 +1,8 @@
+password=armageddon391
+
+echo -n 'armageddon391' | openssl dgst -sha512
+
+john --wordlist=/usr/share/wordlists/rockyou.txt myhash --format=raw-sha512
+
+60aa4d765ead6dcf8248681c5a444502fa23f9c26fd8885d7ae2571ee67558c6e6b1b33c2c6450d443baeadb3413058c5eeeb67d87f88da9584afa6c4a130586
+
